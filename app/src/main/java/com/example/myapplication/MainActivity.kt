70cbaab625a671021b16fc9d3dac31d214ca3580
Sokolov N.V <Sokolov_NV@open.ru>
2021-12-02 17:58:59 +0300

    Небольшое приложение для рандомных испытаний

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.tvChallenges.setOnClickListener{
+            binding.tvChallenges.text = getChallenges()
+        }
+    }
+
+    private fun getChallenges() : String{
+        return resources.getStringArray(R.array.challenges)[randomNumber()]
+    }
+
+    private fun randomNumber () : Int{
+        val size = resources.getStringArray(R.array.challenges).size - 1
+        return (0..size).random()
+    }
+}
